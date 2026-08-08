d935990728a7b45e076bf4118da18cea0f7c44ca
Cyber-Punk2001 <saudylebron54@gmail.com>
2026-08-08 16:41:51 +0000

    actualizacion para vercel

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
 
-# Crear la aplicación Flask
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="public",
+    static_url_path="/static"
+)
 
-# Ruta principal
+
 @app.route("/")
 def inicio():
     return render_template("index.html")
 
-# Ejecutar el servidor
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
